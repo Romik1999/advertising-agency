5a6264c0bfee54bd3a6eb8dd56fce19b574444d1
roman <usankin.roma@gmail.com>
2023-05-30 13:18:34 +0400

    fix styles in modals

--- src/js/app.js
+++ src/js/app.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 
-    if (heroButton){
+    if (heroButton) {
         heroButton.addEventListener('click', () => {
             servicesInput.value = '';
         })
@@ -231,3 +231,95 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 });
+
+// показ окошек с благодарностью после отправки форм
+// показ окошек с благодарностью после отправки форм
+let form = document.querySelectorAll('.wpcf7-form'),
+    thanksModal = document.querySelector('.thanks'),
+    thanksModalClose = document.querySelector('.thanks__close');
+
+form.forEach(item => {
+    item.addEventListener('wpcf7mailsent', function (event) {
+        Fancybox.close();
+        thanksModal.classList.add('thanks--active');
+        setTimeout(function () {
+            thanksModal.classList.remove('thanks--active');
+        }, 2500);
+    }, false);
+});
+
+thanksModalClose.addEventListener('click', ()=>{
+    thanksModal.classList.remove('thanks--active');
+})
+
+
+
+let phoneInputs = document.querySelectorAll('input[type="tel"]');
+
+if (phoneInputs) {
+    phoneInputs.forEach(item => {
+        item.setAttribute('onkeypress', 'maskPhone(event)');
+        item.setAttribute('onpaste', 'onPastePhone(event)');
+    });
+}
+
+function maskPhone(e) {
+    const mask = /\+7 \(\d{3}\) \d{3} \d{2} \d{2}/;
+    var valSize = e.target.value.trim().replace(/\D/g, "").length;
+    e = e || window.event;
+    var key = e.keyCode || e.which;
+    key = String.fromCharCode(key);
+    var regex = /[0-9]|\+/;
+    if (!regex.test(key)) {
+        e.returnValue = false;
+        if (e.preventDefault) e.preventDefault();
+    } else {
+        if (valSize !== 0 && key === "+") {
+            e.returnValue = false;
+            return;
+        }
+        if (valSize === 0) {
+            if (key === "8" || key === "7") {
+                e.target.value = "+7";
+                e.returnValue = false;
+            } else if (key === "9") {
+                e.target.value = "+7 (9";
+                e.returnValue = false;
+            } else if (key !== "+") {
+                e.target.value = "+7 (9";
+            } else if (key === "+" && e.target.value === "+") {
+                e.returnValue = false;
+            }
+        } else if (valSize === 1) {
+            e.target.value = "+7 (9";
+            if (key === "9") {
+                e.returnValue = false;
+            }
+        } else if (valSize === 4) {
+            if (e.target.value.slice(-1) === ")") {
+                e.target.value = e.target.value.trim() + " ";
+            } else if (e.target.value.slice(-1) === " ") {
+                return;
+            } else e.target.value = e.target.value.trim() + ") ";
+        } else if (valSize === 7 || valSize === 9) {
+            if (e.target.value.slice(-1) === " ") {
+                return;
+            } else e.target.value = e.target.value.trim() + " ";
+        } else if (valSize === 11) {
+            e.returnValue = false;
+        }
+    }
+}
+
+function onPastePhone(e) {
+    e.preventDefault();
+    const mask = /(7|8)(9\d{2})(\d{3})(\d{2})(\d{2})/;
+    var phone = e.clipboardData.getData('text/plain').replace(/\D/g, "");
+    if (!mask.test(phone)) {
+        e.returnValue = false;
+        return;
+    }
+    var matched = phone.match(mask);
+    e.target.value = "+7 (" + matched[2] + ") " + matched[3] + " " + matched[4] + " " + matched[5];
+    e.returnValue = false;
+}
